d39bb703275d346afe47d034e019ef0658e32fdf
davidvegacorus <david.vega@corusconsulting.com>
2025-07-02 17:54:28 -0500

    Feature: Mejoras visuales

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tumbesfc-media.s3.us-east-2.amazonaws.com/meta/maya.jpeg', // Reemplaza esto con la URL de tu sitio
   integrations: [tailwindcss(), react()]
 });
